6fb8cb45681e8280aa6b653e735a389f5dba1f5a
GrayyFoxx <ewangxev237@aol.com>
2023-12-12 06:13:21 -0500

    Conc

---
+++ Level 2/multiple tfvars/provider.tf
@@ -0,0 +1,7 @@
+# provider "aws" {
+#   region  = var.region
+#   profile = var.profile
+# }
+
+
+# Makes sure to remove the comments from this before running. Else it won't work.
---
+++ Level 2/multiple tfvars/vars/test.tfvars
@@ -0,0 +1,9 @@
+ami_id="ami-018ba43095ff50d08"
+
+instance_type="t2.micro"
+
+ec2_name_tag="dev-instance"
+
+region = "us-east-1"
+
+profile = "default"
